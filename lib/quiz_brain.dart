d052f584e52bc0fdee065455a2fe0ce98ef47f50
Jasdeep Singh <cingh.jasdeep@gmail.com>
2021-11-03 11:42:30 +0530

    Waheguru start

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -37,17 +37,17 @@ class QuizBrain {
     }
   }
 
-  String getQuestionText() {
-    return _questionBank[_questionNumber].questionText;
-  }
+  String getQuestionText() => _questionBank[_questionNumber].questionText;
 
-  bool getCorrectAnswer() {
-    return _questionBank[_questionNumber].questionAnswer;
-  }
+  bool getCorrectAnswer() => _questionBank[_questionNumber].questionAnswer;
 
   //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
 
   //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+  bool isFinished() {
+    print(_questionNumber == (_questionBank.length - 1)); //13, 0 - 12
+    return _questionNumber == (_questionBank.length - 1);
+  }
 
   //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
 }
